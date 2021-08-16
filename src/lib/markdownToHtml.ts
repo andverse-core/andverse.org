72190389333049f763ebe16df0f448be013efa6b
arisac <hi@aris.ac>
2021-08-16 00:34:04 -0700

    remove Docs from project

--- src/lib/markdownToHtml.ts
+++
@@ -1,8 +0,0 @@
-import remark from 'remark'
-import html from 'remark-html'
-import remarkGfm from 'remark-gfm'
-
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
-}

5ea6e7780291c59048526fdf4c6489101ba4d708
arisac <hi@aris.ac>
2021-09-05 19:20:06 -0700

    remove docs link

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const footerNavigation = {
   main: [
     { name: 'Home', href: '/' },
-    { name: 'Documentation', href: 'https://docs.andromeda.org' }
+    { name: 'Documentation', href: '/#' }
   ],
   social: [
     {

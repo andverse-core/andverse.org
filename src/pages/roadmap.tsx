08f18372f2d79864bacaef572734792357078dca
weixuefeng <weixuefeng@diynova.com>
2021-11-14 22:18:28 +0800

    update roadmap style

--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -4,13 +4,6 @@ import Footer from 'layouts/Footer'
 import Header from 'layouts/Header'
 
 class Home extends Component {
-  componentDidMount() {
-    const script = document.createElement('script')
-    script.async = true
-    script.src = '/assets/sf-init.js'
-    document.body.appendChild(script)
-  }
-
   render() {
     return (
       <div id="page-roadmap">
@@ -58,9 +51,9 @@ function Roadmap() {
               <div className="cd-timeline-img cd-location"></div>
               <div className="cd-timeline-content">
                 <h3>2021 Q4</h3>
-                <p>Andverse market launched</p>
-                <p>Land Mint Round #1</p>
-                <p>Build the 2D/3D map of Andverse Planet #3</p>
+                <p>1. Andverse market launched</p>
+                <p>2. Land Mint Round #1</p>
+                <p>3. Build the 2D/3D map of Andverse Planet #3</p>
               </div>
             </div>
 
@@ -68,10 +61,10 @@ function Roadmap() {
               <div className="cd-timeline-img cd-location"></div>
               <div className="cd-timeline-content">
                 <h3>2022 Q1</h3>
-                <p>World Environment & Buildings Development</p>
-                <p>Lands-Estates Manager Launch</p>
-                <p>New Andverse Map Generator</p>
-                <p>3D Assets Market</p>
+                <p>1. World Environment & Buildings Development</p>
+                <p>2. Lands-Estates Manager Launch</p>
+                <p>3. New Andverse Map Generator</p>
+                <p>4. 3D Assets Market</p>
               </div>
             </div>
 
@@ -79,10 +72,10 @@ function Roadmap() {
               <div className="cd-timeline-img cd-location"></div>
               <div className="cd-timeline-content">
                 <h3>2022 Q2</h3>
-                <p>Andverse World 3D Dev Preview</p>
-                <p>Andverse World 1.0 Launch, with VR support</p>
-                <p>3D Assets Manager</p>
-                <p>Andverse 3D Buildings Growth</p>
+                <p>1. Andverse World 3D Dev Preview</p>
+                <p>2. Andverse World 1.0 Launch, with VR support</p>
+                <p>3. 3D Assets Manager</p>
+                <p>4. Andverse 3D Buildings Growth</p>
               </div>
             </div>
 
@@ -90,10 +83,10 @@ function Roadmap() {
               <div className="cd-timeline-img cd-location"></div>
               <div className="cd-timeline-content">
                 <h3>2022 Q3</h3>
-                <p>MMO Players Link Launch</p>
-                <p>DID Standard</p>
-                <p>Avatar Market Launch</p>
-                <p>Custom Avatars</p>
+                <p>1. MMO Players Link Launch</p>
+                <p>2. DID Standard</p>
+                <p>3. Avatar Market Launch</p>
+                <p>4. Custom Avatars</p>
               </div>
             </div>
 
@@ -101,9 +94,9 @@ function Roadmap() {
               <div className="cd-timeline-img cd-location"></div>
               <div className="cd-timeline-content">
                 <h3>2022 Q4</h3>
-                <p>Andverse World API & SDK Release</p>
-                <p>DAO Launch</p>
-                <p>Andverse World Builder DApp</p>
+                <p>1. Andverse World API & SDK Release</p>
+                <p>2. DAO Launch</p>
+                <p>3. Andverse World Builder DApp</p>
               </div>
             </div>
           </section>

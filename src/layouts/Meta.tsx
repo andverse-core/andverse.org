2f583f1cd33e987285a2e136b802d2af3a19e7d5
weixuefeng <weixuefeng1018@gmail.com>
2021-11-29 15:05:44 +0800

    add mailchimp config

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -12,6 +12,7 @@ const Meta = () => {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" content="#000" />
       <meta name="description" content={`${SITE_NAME}.`} />
+      <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/1511a1328f18eb993d0169316/77d57dd3f35939b45e6ba021c.js");</script>
     </Head>
   )
 }
